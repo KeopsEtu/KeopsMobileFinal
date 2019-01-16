84b9f51d9f1c5dedf2a4fbc5e659361cd964e640
yalimbilgin <yalimbilgin@gmail.com>
2019-01-16 18:27:42 +0300

    Added menu for main page

--- app/src/main/java/com/example/user/keops/feedActivity.java
+++ app/src/main/java/com/example/user/keops/feedActivity.java
@@ -1,9 +1,29 @@
 package com.example.user.keops;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class feedActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuinflater = getMenuInflater();
+        menuinflater.inflate(R.menu.listener,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.listener) {
+            Intent intent =  new Intent(getApplicationContext(), listenActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

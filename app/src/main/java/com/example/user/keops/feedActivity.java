13be44a7d742a14b43535be5ef896b3e5399894c
yalimbilgin <yalimbilgin@gmail.com>
2019-01-19 17:04:06 +0300

    Added database get the list

--- app/src/main/java/com/example/user/keops/feedActivity.java
+++ app/src/main/java/com/example/user/keops/feedActivity.java
@@ -1,13 +1,32 @@
 package com.example.user.keops;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class feedActivity extends AppCompatActivity {
+
+    ListView listView;
+    postClass adapter;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    ArrayList<String> listItemFromFB;
+    private FirebaseAuth mAuth;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,5 +52,34 @@ public class feedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+        listView = findViewById(R.id.listView);
+        listItemFromFB = new ArrayList<>();
+        adapter = new postClass(listItemFromFB,listItemFromFB,this);
+        listView.setAdapter(adapter);
+        mAuth = FirebaseAuth.getInstance();
+        getDataFromFirebase();
+    }
+
+    public void getDataFromFirebase() {
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    HashMap<String,String> hashMap = (HashMap<String, String>) ds.getValue();
+                    if(mAuth.getCurrentUser().getEmail().equals(hashMap.get("userEmail"))) {
+                        listItemFromFB.add(hashMap.get("item"));
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }

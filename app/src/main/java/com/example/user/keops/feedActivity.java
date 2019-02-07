0228fba3d9a6d693430fa22a0f75e3b29adf7b7b
yalimbilgin <yalimbilgin@gmail.com>
2019-02-07 10:46:25 +0300

    Search function for list is handled

--- app/src/main/java/com/example/user/keops/feedActivity.java
+++ app/src/main/java/com/example/user/keops/feedActivity.java
@@ -1,6 +1,9 @@
 package com.example.user.keops;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +15,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -45,6 +50,12 @@ public class feedActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuinflater = getMenuInflater();
         menuinflater.inflate(R.menu.listener, menu);
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -69,6 +80,7 @@ public class feedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        handleIntent(getIntent());
 
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference();
@@ -81,6 +93,27 @@ public class feedActivity extends AppCompatActivity {
         delete = new ArrayList<>();
         temp = new ArrayList<>();
         getDataFromFirebase();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            final String query = intent.getStringExtra(SearchManager.QUERY);
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if(adapter!= null)
+                        adapter.getFilter().filter(query);
+                }
+            }, 10);
+
+        }
     }
 
     public void getDataFromFirebase() {

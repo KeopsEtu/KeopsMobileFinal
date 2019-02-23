a046265b7074d60c74240a6c14ca8444173337f6
yalimbilgin <yalimbilgin@gmail.com>
2019-02-23 12:26:00 +0300

    Main page analyse change

--- app/src/main/java/com/example/user/keops/ListViewAdapter.java
+++ app/src/main/java/com/example/user/keops/ListViewAdapter.java
@@ -3,10 +3,12 @@ package com.example.user.keops;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -32,6 +34,7 @@ public class ListViewAdapter extends ArrayAdapter<String> {
         ViewHolder holder;
         LayoutInflater inflater = (LayoutInflater) activity
                 .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
         // If holder not exist then locate all view from UI file.
         if (convertView == null) {
             // inflate UI from XML file
@@ -50,6 +53,15 @@ public class ListViewAdapter extends ArrayAdapter<String> {
         //handling buttons event
         holder.btnEdit.setOnClickListener(onEditListener(position, holder));
         holder.btnDelete.setOnClickListener(onDeleteListener(position, holder));
+        final String temp = getItem(position);
+        holder.btnAnalyse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(activity.getApplicationContext(), analysisActivity.class);
+                i.putExtra("send_string",temp);
+                activity.startActivity(i);
+            }
+        });
 
         return convertView;
     }
@@ -120,6 +132,7 @@ public class ListViewAdapter extends ArrayAdapter<String> {
     private class ViewHolder {
         private TextView name;
         private View btnDelete;
+        private View btnAnalyse;
         private View btnEdit;
         private SwipeLayout swipeLayout;
 
@@ -127,6 +140,7 @@ public class ListViewAdapter extends ArrayAdapter<String> {
             swipeLayout = (SwipeLayout) v.findViewById(R.id.swipe_layout);
             btnDelete = v.findViewById(R.id.delete);
             btnEdit = v.findViewById(R.id.edit_query);
+            btnAnalyse = v.findViewById(R.id.analyse);
             name = (TextView) v.findViewById(R.id.name);
 
             swipeLayout.setShowMode(SwipeLayout.ShowMode.LayDown);

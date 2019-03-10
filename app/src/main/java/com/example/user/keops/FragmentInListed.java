2076b52ae1c45acbe918c19cb766e0f8bab5f4e6
ahmet <ahmetgokhansener@gmail.com>
2019-03-10 21:01:06 +0300

    TabBar için java classları oluşturuldu.

---
+++ app/src/main/java/com/example/user/keops/FragmentInListed.java
@@ -0,0 +1,25 @@
+package com.example.user.keops;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FragmentInListed extends Fragment {
+
+    TextView textView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_Inlist, container, false);
+        textView = (TextView) view.findViewById(R.id.fragmentInListTextView);
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
